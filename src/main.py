7c3152f435c0dafccc31bf81cac91dfd0c1857b4
Cyb3r Blaze <cyb3rblaze@gmail.com>
2020-02-13 11:10:13 -0800

    model architecture update

--- src/main.py
+++ src/main.py
@@ -9,31 +9,31 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
         "../data/train",
-        target_size=(256, 256),
+        target_size=(64, 64),
         batch_size=20,
         class_mode='binary')
 
 test_generator = test_datagen.flow_from_directory(
         "../data/test",
-        target_size=(256, 256),
+        target_size=(64, 64),
         batch_size=5,
         class_mode='binary')
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Input(shape=(256, 256, 3)))
+model.add(tf.keras.layers.Input(shape=(64, 64, 3)))
 
-model.add(tf.keras.layers.Conv2D(16, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
+model.add(tf.keras.layers.Conv2D(5, 3, activation='tanh'))
+model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
 
-model.add(tf.keras.layers.Conv2D(32, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
-
-model.add(tf.keras.layers.Conv2D(64, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
+model.add(tf.keras.layers.Conv2D(5, 3, activation='tanh'))
+model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
 
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(120, activation='tanh'))
+model.add(tf.keras.layers.Dense(84, activation='tanh'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
@@ -48,7 +48,7 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_o
 history = model.fit_generator(
       train_generator,
       steps_per_epoch=100,
-      epochs=10,
+      epochs=50,
       validation_data=test_generator,
       validation_steps=50,
       verbose=1,
--- src/model.py
+++ src/model.py
@@ -4,26 +4,26 @@ import numpy as np
 
 model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Input(shape=(256, 256, 3)))
+model.add(tf.keras.layers.Input(shape=(64, 64, 3)))
 
-model.add(tf.keras.layers.Conv2D(16, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
+model.add(tf.keras.layers.Conv2D(5, 3, activation='tanh'))
+model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
 
-model.add(tf.keras.layers.Conv2D(32, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
-
-model.add(tf.keras.layers.Conv2D(64, 3, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(2))
+model.add(tf.keras.layers.Conv2D(5, 3, activation='tanh'))
+model.add(tf.keras.layers.AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
 
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(120, activation='tanh'))
+model.add(tf.keras.layers.Dense(84, activation='tanh'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 model.load_weights("training_1/cp.ckpt")
 
 evaluate_image=Image.open("../output/dog.jpg")
-evaluate_image=evaluate_image.resize((256, 256), Image.ANTIALIAS)
-evaluate_image=np.array(evaluate_image).reshape((1, 256, 256, 3))
+evaluate_image=evaluate_image.resize((64, 64), Image.ANTIALIAS)
+evaluate_image=np.array(evaluate_image).reshape((1, 64, 64, 3))
 value=model.predict(evaluate_image)
 
 print(value)
